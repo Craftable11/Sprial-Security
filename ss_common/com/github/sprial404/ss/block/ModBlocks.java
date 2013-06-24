fd49cf1330f18779fc15ea96f72bf47a338d5620
Sprial404 <poptropica@westnet.com.au>
2013-06-24 21:49:52 +0800

    Starting on Blocks

---
+++ ss_common/com/github/sprial404/ss/block/ModBlocks.java
@@ -0,0 +1,14 @@
+package com.github.sprial404.ss.block;
+
+public class ModBlocks {
+
+    /* Mod block instances */
+    
+    public static void init() {
+        initBlockRecipes();
+    }
+    
+    private static void initBlockRecipes() {
+        
+    }
+}
