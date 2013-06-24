70c969689e863ddd0cf4eb03ee4aad927378007b
Sprial404 <poptropica@westnet.com.au>
2013-06-24 21:49:27 +0800

    Adding In mcmod.info

---
+++ ss_common/com/github/sprial404/ss/lib/Strings.java
@@ -0,0 +1,31 @@
+package com.github.sprial404.ss.lib;
+
+public class Strings {
+
+    /* General keys */
+    public static final String TRUE = "true";
+    public static final String FALSE = "false";
+    
+    /* Fingerprint check related constants */
+    public static final String INVALID_FINGERPRINT_MESSAGE = "The copy of Sprial Security that you are running has been modified from the original, and unpredictable things may happen. Please consider re-downloading the original version of the mod.";
+    
+    /* Version check related constants */
+    public static final String VERSION_CHECK_INIT_LOG_MESSAGE = "version.init_log_message";
+    public static final String UNINITIALIZED_MESSAGE = "version.uninitialized";
+    public static final String CURRENT_MESSAGE = "version.current";
+    public static final String OUTDATED_MESSAGE = "version.outdated";
+    public static final String GENERAL_ERROR_MESSAGE = "version.general_error";
+    public static final String FINAL_ERROR_MESSAGE = "version.final_error";
+    public static final String MC_VERSION_NOT_FOUND = "version.mc_version_not_found";
+    
+    /* NBT related constants */
+    public static final String NBT_ITEM_DISPLAY = "display";
+    public static final String NBT_ITEM_COLOR = "color";
+    public static final String NBT_TE_STATE_KEY = "teState";
+    public static final String NBT_TE_DIRECTION_KEY = "teDirection";
+    public static final String NBT_TE_CUSTOM_NAME = "CustomName";
+    
+    /* TileEntity name constants */
+    
+    /* Container related constants */
+}
