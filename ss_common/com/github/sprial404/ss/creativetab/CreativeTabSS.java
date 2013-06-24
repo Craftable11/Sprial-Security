f975dca06f29696262d7b07141f69354aa206244
Sprial404 <poptropica@westnet.com.au>
2013-06-24 21:50:18 +0800

    Created Custom Creative Tab

--- ss_common/com/github/sprial404/ss/creativetab/CreativeTabSS.java
+++ ss_common/com/github/sprial404/ss/creativetab/CreativeTabSS.java
@@ -16,6 +16,6 @@ public class CreativeTabSS extends CreativeTabs {
      * the itemID for the item to be displayed on the tab
      */
     public int getTabIconItemIndex() {
-        return 55;
+        return 331;
     }
 }
