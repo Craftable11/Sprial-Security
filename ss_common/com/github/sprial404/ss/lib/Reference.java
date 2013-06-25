cf74fd333f714edfcb34844d82bc469f869ff7f7
Sprial404 <poptropica@westnet.com.au>
2013-06-25 18:05:17 +0800

    Update of References

--- ss_common/com/github/sprial404/ss/lib/Reference.java
+++ ss_common/com/github/sprial404/ss/lib/Reference.java
@@ -13,6 +13,7 @@ public class Reference {
     public static final String DEPENDENCIES = "required-after:Forge@[7.7.1.650,)";
     public static final String FINGERPRINT = "@FINGERPRINT@";
     public static final int SECOND_IN_TICKS = 20;
+    public static final int SHIFTED_ID_RANGE_CORRECTION = 256;
     public static final String SERVER_PROXY_CLASS = "com.github.sprial404.ss.core.proxy.CommonProxy";
     public static final String CLIENT_PROXY_CLASS = "com.github.sprial404.ss.core.proxy.ClientProxy";
     public static final int VERSION_CHECK_ATTEMPTS = 3;
