de753ddb4e513b4d4750fd18088d56a2aaee3a06
Sprial404 <poptropica@westnet.com.au>
2013-06-25 18:08:53 +0800

    Sounds File

---
+++ ss_common/com/github/sprial404/ss/lib/Sounds.java
@@ -0,0 +1,12 @@
+package com.github.sprial404.ss.lib;
+
+public class Sounds {
+
+    @SuppressWarnings("unused")
+    private static final String SOUND_RESOURCE_LOCATION = "mods/ss/sound/";
+    @SuppressWarnings("unused")
+    private static final String SOUND_PREFIX = "mods.ss.sound.";
+    
+    public static String[] soundFiles = { };
+    
+}
