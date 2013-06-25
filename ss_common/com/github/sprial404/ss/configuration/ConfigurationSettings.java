c84cdf2a6d12ef500691b160cd5ed42fa19aae8a
Sprial404 <poptropica@westnet.com.au>
2013-06-25 18:05:45 +0800

    Support for Configurations

---
+++ ss_common/com/github/sprial404/ss/configuration/ConfigurationSettings.java
@@ -0,0 +1,41 @@
+package com.github.sprial404.ss.configuration;
+
+public class ConfigurationSettings {
+
+    /*
+     * Version check related settings
+     */
+    public static boolean DISPLAY_VERSION_RESULT;
+    public static final String DISPLAY_VERSION_RESULT_CONFIGNAME = "version_check.display_results";
+    public static final boolean DISPLAY_VERSION_RESULT_DEFAULT = true;
+
+    public static String LAST_DISCOVERED_VERSION;
+    public static final String LAST_DISCOVERED_VERSION_CONFIGNAME = "version_check.last_discovered_version";
+    public static final String LAST_DISCOVERED_VERSION_DEFAULT = "";
+
+    public static String LAST_DISCOVERED_VERSION_TYPE;
+    public static final String LAST_DISCOVERED_VERSION_TYPE_CONFIGNAME = "version_check.last_discovered_version_type";
+    public static final String LAST_DISCOVERED_VERSION_TYPE_DEFAULT = "";
+    
+    /*
+     * Audio config settings
+     */
+    public static String ENABLE_SOUNDS;
+    public static final String ENABLE_SOUNDS_CONFIGNAME = "sounds.enabled";
+    public static final String ENABLE_SOUNDS_DEFAULT = "all";
+    
+    /*
+     * Graphic config settings
+     */
+    // Whether or not EE3 particle fx are enabled
+    public static boolean ENABLE_PARTICLE_FX;
+    public static final String ENABLE_PARTICLE_FX_CONFIGNAME = "particle_fx.enabled";
+    public static final boolean ENABLE_PARTICLE_FX_DEFAULT = true;
+    
+    /*
+     * Keybinding related config settings
+     */
+    // Toggle key
+    public static final String KEYBINDING_TOGGLE = "key.toggle";
+    public static final int KEYBINDING_TOGGLE_DEFAULT = 34;
+}
