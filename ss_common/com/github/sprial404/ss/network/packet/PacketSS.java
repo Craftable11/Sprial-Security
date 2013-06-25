1402e3082a4a685f632998dae725dcc59fdb9dd0
Sprial404 <poptropica@westnet.com.au>
2013-06-25 18:08:43 +0800

    Packets Baked With Code

---
+++ ss_common/com/github/sprial404/ss/network/packet/PacketSS.java
@@ -0,0 +1,61 @@
+package com.github.sprial404.ss.network.packet;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import net.minecraft.network.INetworkManager;
+
+import com.github.sprial404.ss.network.PacketTypeHandler;
+
+import cpw.mods.fml.common.network.Player;
+
+public class PacketSS {
+
+    public PacketTypeHandler packetType;
+    public boolean isChunkDataPacket;
+    
+    public PacketSS(PacketTypeHandler packetType, boolean isChunkDataPacket) {
+        this.packetType = packetType;
+        this.isChunkDataPacket = isChunkDataPacket;
+    }
+    
+    public byte[] populate() {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(bos);
+        
+        try {
+            dos.writeByte(packetType.ordinal());
+            this.writeData(dos);
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
+        }
+        
+        return bos.toByteArray();
+    }
+    
+    public void readPopulate(DataInputStream data) {
+        try {
+            this.readData(data);
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    public void readData(DataInputStream data) throws IOException {
+        
+    }
+    
+    public void writeData(DataOutputStream dos) throws IOException {
+        
+    }
+    
+    public void execute(INetworkManager network, Player player) {
+
+    }
+
+    public void setKey(int key) {
+
+    }
+}
