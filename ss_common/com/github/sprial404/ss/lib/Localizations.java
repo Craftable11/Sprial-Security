073cf0a7c6e3fd4e67a49a476a7f95a6707afb33
Sprial404 <poptropica@westnet.com.au>
2013-06-25 18:09:08 +0800

    Localization File

---
+++ ss_common/com/github/sprial404/ss/lib/Localizations.java
@@ -0,0 +1,8 @@
+package com.github.sprial404.ss.lib;
+
+public class Localizations {
+
+    private static final String LANG_RESOURCE_LOCATION = "/mods/ss/lang/";
+    
+    public static String[] localeFiles = { LANG_RESOURCE_LOCATION + "en_US.xml" };
+}
