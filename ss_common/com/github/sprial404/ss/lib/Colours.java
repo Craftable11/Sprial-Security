767d1721390c9a9fbdf979761363151406996aa6
Sprial404 <poptropica@westnet.com.au>
2013-06-25 18:09:25 +0800

    Colours o' Plenty

---
+++ ss_common/com/github/sprial404/ss/lib/Colours.java
@@ -0,0 +1,17 @@
+package com.github.sprial404.ss.lib;
+
+public class Colours {
+
+    public static final String PURE_WHITE = "ffffff";
+    
+    /* Text colour related constants */
+    public static final String TEXT_COLOUR_PREFIX_YELLOW = "\u00a7e";
+    public static final String TEXT_COLOUR_PREFIX_GRAY = "\u00a77";
+    public static final String TEXT_COLOUR_PREFIX_WHITE = "\u00a7f";
+    public static final String TEXT_COLOUR_PREFIX_BLUE = "\u00a71";
+    public static final String TEXT_COLOUR_PREFIX_BLACK = "\u00a70";
+    public static final String TEXT_COLOUR_PREFIX_ORANGE = "\u00a76";
+    public static final String TEXT_COLOUR_PREFIX_GREEN = "\u00a72";
+    public static final String TEXT_COLOUR_PREFIX_CYAN = "\u00a7b";
+    public static final String TEXT_COLOUR_PREFIX_MAGENTA = "\u00a75";
+}
