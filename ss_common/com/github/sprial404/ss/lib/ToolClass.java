9c247546bb5d77dabfe6b644e9fec376d66b78fb
Sprial404 <poptropica@westnet.com.au>
2013-07-04 19:13:43 +0800

    Created ToolClasses

---
+++ ss_common/com/github/sprial404/ss/lib/ToolClass.java
@@ -0,0 +1,10 @@
+package com.github.sprial404.ss.lib;
+
+public class ToolClass {
+
+    public static final String PICKAXE = "pickaxe";
+    public static final String AXE = "axe";
+    public static final String SHOVEL = "shovel";
+    public static final String HOE = "hoe";
+    public static final String SWORD = "sword";
+}
