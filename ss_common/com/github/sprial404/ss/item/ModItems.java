51232f3180dd3b501bbf0f399752acbc8b995fc8
Sprial404 <poptropica@westnet.com.au>
2013-07-04 18:59:41 +0800

    Items. /*No Comment*/

--- ss_common/com/github/sprial404/ss/item/ModItems.java
+++ ss_common/com/github/sprial404/ss/item/ModItems.java
@@ -1,10 +1,19 @@
 package com.github.sprial404.ss.item;
 
+import com.github.sprial404.ss.lib.ItemIds;
+
+import net.minecraft.item.Item;
+
+
 public class ModItems {
 
     /* Mod item instances */
+    public static Item plasmaIngot;
+    public static Item plasmaCell;
     
     public static void init() {
-        
+        /* Initialize each mod item individually */
+        plasmaIngot = new ItemPlasmaIngot(ItemIds.PLASMA_INGOT);
+        plasmaCell = new ItemPlasmaCell(ItemIds.PLASMA_CELL);
     }
 }
