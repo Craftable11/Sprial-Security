202cbcb7616feea53403492bf64b21560b2f37af
Sprial404 <poptropica@westnet.com.au>
2013-07-09 21:36:57 +0800

    Gem Block & Ore Ids

--- ss_common/com/github/sprial404/ss/lib/BlockIds.java
+++ ss_common/com/github/sprial404/ss/lib/BlockIds.java
@@ -1,14 +1,22 @@
 package com.github.sprial404.ss.lib;
 
+/**
+ * Sprial-Security
+ * 
+ * BlockIds
+ * 
+ * @author Sprial404
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ */
 public class BlockIds {
 
     /* Default block ids */
-    public static int TOPAZ_ORE_DEFAULT = 2900;
-    public static int TOPAZ_BLOCK_DEFAULT = 2901;
+    public static int GEM_ORE_DEFAULT = 2900;
+    public static int GEM_BLOCK_DEFAULT = 2901;
     public static int MACHINE_STRUCTURE_DEFAULT = 2902;
     
     /* Current block ids */
-    public static int TOPAZ_ORE;
-    public static int TOPAZ_BLOCK;
+    public static int GEM_ORE;
+    public static int GEM_BLOCK;
     public static int MACHINE_STRUCTURE;
 }
