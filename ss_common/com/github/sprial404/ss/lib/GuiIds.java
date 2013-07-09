9fdfad63edf704812df3a7299f0cc54da6ae82e1
Sprial404 <poptropica@westnet.com.au>
2013-07-09 21:32:38 +0800

    New Gui Ids File

---
+++ ss_common/com/github/sprial404/ss/lib/GuiIds.java
@@ -0,0 +1,14 @@
+package com.github.sprial404.ss.lib;
+
+/**
+ * Sprial-Security
+ * 
+ * GuiId
+ * 
+ * @author Sprial404
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ */
+public class GuiIds {
+
+    public static final int FINGERPRINT_SCANNER = 0;
+}
