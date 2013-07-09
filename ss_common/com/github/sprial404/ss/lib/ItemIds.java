e717e0ff3433107c8990abb8eca5152509dbc75e
Sprial404 <poptropica@westnet.com.au>
2013-07-09 21:36:34 +0800

    Gem Id Created

--- ss_common/com/github/sprial404/ss/lib/ItemIds.java
+++ ss_common/com/github/sprial404/ss/lib/ItemIds.java
@@ -1,10 +1,18 @@
 package com.github.sprial404.ss.lib;
 
+/**
+ * Sprial-Security
+ * 
+ * ItemIds
+ * 
+ * @author Sprial404
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ */
 public class ItemIds {
 
     /* Default item ids */
-    public static int TOPAZ_GEM_DEFAULT = 29000;
+    public static int GEM_DEFAULT = 29000;
     
     /* Current item ids */
-    public static int TOPAZ_GEM;
+    public static int GEM;
 }
