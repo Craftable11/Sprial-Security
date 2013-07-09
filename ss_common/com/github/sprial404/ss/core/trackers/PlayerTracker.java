cf87bbe0cd000032af1844dcfe2635b913bed52e
Sprial404 <poptropica@westnet.com.au>
2013-07-09 21:34:02 +0800

    For Tracking Players logging

---
+++ ss_common/com/github/sprial404/ss/core/trackers/PlayerTracker.java
@@ -0,0 +1,31 @@
+package com.github.sprial404.ss.core.trackers;
+
+import com.github.sprial404.ss.security.Fingerprint;
+
+import net.minecraft.entity.player.EntityPlayer;
+import cpw.mods.fml.common.IPlayerTracker;
+
+/**
+ * Sprial-Security
+ * 
+ * PlayerTracker
+ * 
+ * @author Sprial404
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ */
+public class PlayerTracker implements IPlayerTracker {
+    
+    @Override
+    public void onPlayerLogin(EntityPlayer player) {
+        Fingerprint.onPlayerLogin(player);
+    }
+
+    @Override
+    public void onPlayerLogout(EntityPlayer player) { }
+
+    @Override
+    public void onPlayerChangedDimension(EntityPlayer player) { }
+
+    @Override
+    public void onPlayerRespawn(EntityPlayer player) { }
+}
