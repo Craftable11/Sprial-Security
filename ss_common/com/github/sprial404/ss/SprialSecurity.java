61deae0a6aabc977540ece2c57dcf0795704e5a9
Sprial404 <poptropica@westnet.com.au>
2013-08-04 21:20:57 +0800

    Just a Clean Up

--- ss_common/com/github/sprial404/ss/SprialSecurity.java
+++ ss_common/com/github/sprial404/ss/SprialSecurity.java
@@ -10,7 +10,6 @@ import com.github.sprial404.ss.configuration.ConfigurationHandler;
 import com.github.sprial404.ss.core.handlers.LocalizationHandler;
 import com.github.sprial404.ss.core.handlers.VersionCheckTickHandler;
 import com.github.sprial404.ss.core.proxy.CommonProxy;
-import com.github.sprial404.ss.core.util.FingerprintHelper;
 import com.github.sprial404.ss.core.util.LogHelper;
 import com.github.sprial404.ss.core.util.VersionHelper;
 import com.github.sprial404.ss.creativetab.CreativeTabSS;
@@ -42,53 +41,61 @@ import cpw.mods.fml.relauncher.Side;
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
  * 
  */
-@Mod(modid = Reference.MOD_ID, name= Reference.MOD_NAME, version = Reference.VERSION_NUMBER, dependencies = Reference.DEPENDENCIES, certificateFingerprint = Reference.FINGERPRINT)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION_NUMBER, dependencies = Reference.DEPENDENCIES, certificateFingerprint = Reference.FINGERPRINT)
 @NetworkMod(channels = { Reference.CHANNEL_NAME }, clientSideRequired = true, serverSideRequired = false, packetHandler = PacketHandler.class)
 public class SprialSecurity {
 
     @Instance(Reference.MOD_ID)
     public static SprialSecurity instance;
-    
+
     @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
     public static CommonProxy proxy;
-    
-    public static CreativeTabs tabsSS = new CreativeTabSS(CreativeTabs.getNextID(), Reference.MOD_ID);
-    
+
+    public static CreativeTabs tabsSS = new CreativeTabSS(
+            CreativeTabs.getNextID(), Reference.MOD_ID);
+
     @EventHandler
     public void invalidFingerprint(FMLFingerprintViolationEvent event) {
-     // Report (log) to the user that the version of Sprial Security they are using has been changed/tampered with
-     LogHelper.severe(Strings.INVALID_FINGERPRINT_MESSAGE);
+        // Report (log) to the user that the version of Sprial Security they are
+        // using has been changed/tampered with
+        LogHelper.severe(Strings.INVALID_FINGERPRINT_MESSAGE);
     }
-    
+
     @EventHandler
     public void serverStarting(FMLServerStartingEvent event) {
         // Initialize the custom commands
         CommandHandler.initCommands(event);
     }
-    
+
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         // Initialize the log helper
         LogHelper.init();
-        
+
         // Load the localization files into the LanguageRegistry
         LocalizationHandler.loadLanguages();
-        
+
         // Initialize the configuration
-        ConfigurationHandler.init(new File(event.getModConfigurationDirectory().getAbsolutePath() + File.separator + Reference.CHANNEL_NAME + File.separator + Reference.MOD_ID + ".cfg"));
-        
+        ConfigurationHandler.init(new File(event.getModConfigurationDirectory()
+                .getAbsolutePath()
+                + File.separator
+                + Reference.CHANNEL_NAME
+                + File.separator + Reference.MOD_ID + ".cfg"));
+
         // Conduct the version check and log the result
         VersionHelper.execute();
-        
+
         // Initialize the Version Check Tick Handler (Client only)
-        TickRegistry.registerTickHandler(new VersionCheckTickHandler(), Side.CLIENT);
-        
+        TickRegistry.registerTickHandler(new VersionCheckTickHandler(),
+                Side.CLIENT);
+
         // Initialize the Player Movement Tick Handler (Client only)
-        // TickRegistry.registerTickHandler(new PlayerMovementTickHandler(), Side.CLIENT);
-        
+        // TickRegistry.registerTickHandler(new PlayerMovementTickHandler(),
+        // Side.CLIENT);
+
         // Register the Trackers (Server and Client)
         proxy.registerTrackers();
-        
+
         // Initialize the Render Tick Handler (Client only)
         proxy.registerRenderTickHandler();
 
@@ -97,31 +104,28 @@ public class SprialSecurity {
 
         // Register the Sound Handler (Client only)
         proxy.registerSoundHandler();
-        
+
         // Initalize mod blocks
         ModBlocks.init();
-        
+
         // Initialize mod items
         ModItems.init();
-        
-        // Load Fingerprints from File
-       FingerprintHelper.init();
     }
-    
+
     @EventHandler
     public void load(FMLInitializationEvent event) {
         // Register the GUI Handler
         NetworkRegistry.instance().registerGuiHandler(instance, proxy);
-        
+
         // Initialize mod tile entities
         proxy.registerTileEntities();
-        
+
         // Initialize custom rendering and pre-load textures (Client only)
         proxy.initRenderingAndTextures();
     }
-    
+
     @EventHandler
     public void modsLoaded(FMLPostInitializationEvent event) {
-        
+
     }
 }
