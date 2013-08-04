cbc9747329f5e2c6c5650afdeaad3cd5d3d24930
Sprial404 <poptropica@westnet.com.au>
2013-08-04 21:22:26 +0800

    Clean Up and some new NBT Strings

--- ss_common/com/github/sprial404/ss/lib/Strings.java
+++ ss_common/com/github/sprial404/ss/lib/Strings.java
@@ -13,11 +13,10 @@ public class Strings {
     /* General keys */
     public static final String TRUE = "true";
     public static final String FALSE = "false";
-    
+
     /* Fingerprint check related constants */
     public static final String INVALID_FINGERPRINT_MESSAGE = "The copy of Sprial Security that you are running has been modified from the original, and unpredictable things may happen. Please consider re-downloading the original version of the mod.";
-    
-    
+
     /* Version check related constants */
     public static final String VERSION_CHECK_INIT_LOG_MESSAGE = "version.init_log_message";
     public static final String UNINITIALIZED_MESSAGE = "version.uninitialized";
@@ -26,7 +25,7 @@ public class Strings {
     public static final String GENERAL_ERROR_MESSAGE = "version.general_error";
     public static final String FINAL_ERROR_MESSAGE = "version.final_error";
     public static final String MC_VERSION_NOT_FOUND = "version.mc_version_not_found";
-    
+
     /* NBT related constants */
     public static final String NBT_ITEM_DISPLAY = "display";
     public static final String NBT_ITEM_COLOR = "color";
@@ -46,26 +45,33 @@ public class Strings {
     public static final String NBT_ENCODED_ATTR_ITEM_TAG_COMPOUND = "tag";
     public static final String NBT_ENCODED_ATTR_ORE_NAME = "oreName";
     public static final String NBT_ENCODED_ATTR_ENERGY_NAME = "energyName";
-    public static final String NBT_FINGERPRINT = "fingerprint";
-    public static final String NBT_USERNAME = "username";
-    
+    public static final String NBT_FINGERPRINT = "Fingerprint";
+    public static final String NBT_USERNAME = "Username";
+    public static final String NBT_AMOUNT = "Amount";
+    public static final String NBT_BATTERY_TYPE = "BatteryType";
+    public static final String NBT_ENERGY_TYPE = "EnergyType";
+
     /** List of Gems Names */
-    public static final String[] GEM_NAMES = new String[] { "Topaz", "Onyx", "Spinel", "Moonstone", "AndesineLabradorite", "Amethyst", "FireOpal", "Jadeite", "Agate" };
-    
+    public static final String[] GEM_NAMES = new String[] { "Topaz", "Onyx",
+            "Spinel", "Moonstone", "AndesineLabradorite", "Amethyst",
+            "FireOpal", "Jadeite", "Agate" };
+
     /* Block name constants */
     public static final String GEM_ORE_NAME = "oreGem";
     public static final String GEM_BLOCK_NAME = "blockGem";
     public static final String MACHINE_STRUCTURE_NAME = "structureMachine";
     public static final String FINGERPRINT_SCANNER_NAME = "scannerFingerprint";
-    
+
     /* Item name constants */
     public static final String GEM_NAME = "gem";
-    
+    public static final String BATTERY_NAME = "battery";
+
     /* TileEntity name constants */
     public static final String TE_FINGERPRINT_SCANNER_NAME = "tileScannerFingerprint";
-    
+
     /* Container related constants */
-    public static final String CONTAINER_FINGERPRINT_SCANNER_NAME = "container." + FINGERPRINT_SCANNER_NAME;
+    public static final String CONTAINER_FINGERPRINT_SCANNER_NAME = "container."
+            + FINGERPRINT_SCANNER_NAME;
     public static final String CONTAINER_INVENTORY = "container.inventory";
     public static final String CONTAINER_PORTABLE_CRAFTING = "container.crafting";
 }
